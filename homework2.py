a9592d3fca33b6071791d2e1efb4cd6c3c85c1dd
ecsel13 <ecsel13@mail.ru>
2024-07-28 23:31:15 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_count = 12
+hours_count = 1.5
+name_of_course = 'Python'
+hours = hours_count / homework_count
+print('Курс:', str(name_of_course) + ',', 'всего задач:' + str(homework_count) + ',',
+      'затрачено часов:' + str(hours_count) + ',', 'среднее время выполнения ', hours, 'часа.')
